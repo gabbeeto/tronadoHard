fc30fd95a16348fdab491d8e7153985b0634c2fa
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-11-17 15:36:21 -0300

    make structure for the website

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import 'react'
+import 'react-dom'
+import { createRoot } from 'react-dom/client'
+
+
+// let mainRoot = createRoot(document.querySelector('main'));
+// mainRoot.render((<h1>hello</h1>));
