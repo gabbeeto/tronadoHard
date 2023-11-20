8fd9717e90a8fe6db1d0fbd910ff832d1bdd1bbc
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-11-20 15:55:10 -0300

    add mediafire links

--- popup.js
+++ popup.js
@@ -10,22 +10,27 @@ function openDialog() {
     case "0":
       dialog.innerHTML = `
       <div>
-        <button><a href="" download="tronadoHardWindow.zip">descargar Instaladores para window</a></button>
-        <button><a href="" download="tronadoHardWindow.zip">descargar Instaladores para linux</a></button>
-
+<button><a href="" download="tronadoHardWindow.zip">descargar Instaladores para window</a></button>
+<button><a href="" download="tronadoHardWindow.zip">descargar Instaladores para linux</a></button>
         <h2>links externos:</h2>
         <ul>
           <details>
             <summary>
-              Google Drive
+             Mediafire
             </summary>
             <section>
-              <button><a target="_blank"
-                  href="https://drive.google.com/drive/folders/1HKKs15ZVdVkc9Utph76uiq4Hgb1i59fS">windows</a></button>
-              <button><a target="_blank"
-                  href="https://drive.google.com/drive/folders/19gm9GzQu8YsHhwifL3VQ_RNLrOjc_Pa8">linux</a></button>
+              <button><a target="_blank" href="https://www.mediafire.com/file/nclqqzvonlry3i2/tronadoHard_para_window.zip/file">windows</a></button>
+              <button><a target="_blank" href="https://www.mediafire.com/file/mhyltsr9aoe2abo/tronadoHard_para_linux.zip/file">linux</a></button>
             </section>
-
+          </details>
+          <details>
+            <summary>
+              Google drive(con archivos individuales separados)
+            </summary>
+            <section>
+              <button><a target="_blank" href="https://drive.google.com/drive/folders/1HKKs15ZVdVkc9Utph76uiq4Hgb1i59fS">windows</a></button>
+              <button><a target="_blank" href="https://drive.google.com/drive/folders/19gm9GzQu8YsHhwifL3VQ_RNLrOjc_Pa8">linux</a></button>
+            </section>
           </details>
         </ul>
       </div>
