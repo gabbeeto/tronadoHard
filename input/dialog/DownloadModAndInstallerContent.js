3b209551397f5989c080494785c08b813bb6c31a
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-12-31 12:46:48 -0300

    translate the entire download mods and installer pop up

--- input/dialog/DownloadModAndInstallerContent.js
+++ input/dialog/DownloadModAndInstallerContent.js
@@ -8,38 +8,89 @@ function txt(spanishText, englishText) {
 }
 
 
-function ModContainer({ name, description, link, version = false, versionText = "version" }) {
+function ModContainer({ name, description, link, version = false, versionText = "version", reasonWhy = false, nameSpan = false, dependencies = false }) {
   let versionContent;
   if (version) {
     versionContent = (<>
       <dt>{versionText}:</dt>
-      <dd>7.0.0-pre36</dd></>)
+      <dd>{version}</dd></>)
   }
   else {
     versionContent = '';
   }
 
+
+  let reasonWhyContent;
+  if (reasonWhy) {
+    reasonWhyContent = (<>
+      <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
+      <dd>{reasonWhy}</dd></>)
+  }
+  else {
+    reasonWhyContent = '';
+  }
+
+
+  let dependencyContent;
+  if (dependencies) {
+    dependencyContent = (<>
+      <dt>{`${txt("Dependencia", "dependency")}`}:</dt>
+      {dependencies.map(dependency => (<dd>{dependency}</dd>))}</>)
+  }
+  else {
+    dependencyContent = '';
+  }
+
+
+  let nameSpanContent;
+  if (nameSpan) {
+    nameSpanContent = (<span>{nameSpan}</span>)
+  }
+  else {
+    nameSpanContent = '';
+  }
+  let linkContent;
+  if (typeof link == "string") {
+    linkContent = (<><dt>Link:</dt>
+      <dd><a href={link}>{link}</a></dd>
+    </>
+    )
+  }
+
+  else {
+    linkContent = (<><dt>Link:</dt>
+      {link.map(linkContent => {
+        return (
+          <>
+            <dd><a href={linkContent}>{linkContent}</a></dd>
+          </>
+        )
+      })}
+    </>)
+  }
+
   return (<div className="modContainer">
-      <dt>{name}</dt>
-      <dd>
-        <dl>
-          <dt>{`${txt("Descripción", "Description")}`}:</dt>
-          <dd>{description}</dd>
-          {versionContent}
-          <dt>Link:</dt>
-          <dd><a href={link}>{link}</a></dd>
-        </dl>
-      </dd>
-    </div>)
+    <dt>{name}{nameSpanContent}</dt>
+    <dd>
+      <dl>
+        <dt>{`${txt("Descripción", "Description")}`}:</dt>
+        <dd>{description}</dd>
+        {versionContent}
+        {reasonWhyContent}
+        {dependencyContent}
+        {linkContent}
+      </dl>
+    </dd>
+  </div>)
 }
 
 export default function() {
 
   return (<>
     <div>
-      <button><a href="./../zipFiles/windows.zip" download="TronadoHard Para Windows.zip">descargar Instaladores para window</a></button>
-      <button><a href="./../zipFiles/linux.zip" download="TronadoHard Para Linux.zip">descargar Instaladores para linux</a></button>
-      <h2>links externos por Gabbeeto:</h2>
+      <button><a href="./../zipFiles/windows.zip" download="TronadoHard Para Windows.zip">{`${txt("descargar Instaladores y mods para Window", "download installers and mods for Window")}`}</a></button>
+      <button><a href="./../zipFiles/linux.zip" download="TronadoHard Para Linux.zip">{`${txt("descargar Instaladores para linux", "download installers and mods for Linux")}`}</a></button>
+      <h2>{`${txt("enlaces externos", "external links")}`}:</h2>
       <ul>
         <details>
           <summary>
@@ -63,7 +114,7 @@ export default function() {
 
       <h2>{`${txt("Informacion de mods y links de CurseForge", "mod Information and CurseForge Links")}`} : <span> {`${txt("recomendado leer la seccion de mods opcionales para gente que no usa instalador para saber que hace cada mod opcional", "it's recommended to read the mod descriptions from the 'Optional mods' section for people who do not want to use the installer since it's not necessary to install all the mods so you can choose which mod you want more wisely")}`}</span> </h2>
       <details>
-        <summary>mods necesarios para entrar al server</summary>
+        <summary>{`${txt("mods necesarios para entrar al server", "neccesary mods to get into the server")}`}</summary>
         <section>
           <h2>
             {`${txt("todos los mods requeridos son 100% requeridos para entrar al server", "mods that are 100% required to get into the server")}`}</h2>
@@ -183,157 +234,70 @@ export default function() {
             />
 
 
+            <ModContainer
+              name="Xaero's Minimap"
+              description={`${txt("ver el mapa y lo que aparece cerca del mapa y guardar ubicacion", "being able to look at the map and the mobs/player that surround the map and being able to save the locations")}`}
+              link="https://www.curseforge.com/minecraft/mc-mods/xaeros-minimap"
+              reasonWhy={`${txt("Te puede ayudar a ver las cordenadas sin precionar el f3 y es muy conveniente mirar si hay monstruos/jugadores cercas dentro del mapa y es muy conveniente tambien si no tenes buena memoria porque podes guardar cordenadas", "it helps you see the f3 coordenates without pressing f3 and it's convinient if there are monsters/player close to you and it's also convinient if you don't have good memory since you can save up the locations")}`}
+            />
 
-            <div className="modContainer">
-              <dt>Xaero's Minimap</dt>
-              <dd>
-                <dl>
-                  <dt>{`${txt("Descripción", "Description")}`}:</dt>
-                  <dd>{`${txt("ver el mapa y lo que aparece cerca del mapa y guardar ubicacion", "being able to look at the map and the mobs/player that surround the map and being able to save the locations")}`}</dd>
-
-                  <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
-                  <dd>{`${txt("Te puede ayudar a ver las cordenadas sin precionar el f3 y es muy conveniente mirar si hay monstruos/jugadores cercas dentro del mapa y es muy conveniente tambien si no tenes buena memoria porque podes guardar cordenadas", "it helps you see the f3 coordenates without pressing f3 and it's convinient if there are monsters/player close to you and it's also convinient if you don't have good memory since you can save up the locations")}`}</dd>
-
-                  <dt>Link:</dt>
-                  <dd><a href="https://www.curseforge.com/minecraft/mc-mods/xaeros-minimap">https://www.curseforge.com/minecraft/mc-mods/xaeros-minimap</a></dd>
-                </dl>
-              </dd>
-            </div>
-
-            <div className="modContainer">
-              <dt>Just Enough Items</dt>
-              <dd>
-                <dl>
-                  <dt>{`${txt("Descripción", "Description")}`}:</dt>
-                  <dd>{`${txt("Podes ver recipientes avanzados que no te muestra en Vanilla Minecraft y se integra con el mod create muy bien", "you can see advanced recipes that are not shown in vanilla Minecraft and it integrates very well with the create mod")}`}</dd>
-
-                  <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
-                  <dd>{`${txt("Lo vas a necesitar para saber como craftear cosas de mods como el Mod Create o el mod de decoracion", "You're going to need this if you're going to craft things that are related to some of the mods like the create/furniture mod")}`}</dd>
-
-                  <dt>Link:</dt>
-                  <dd><a href="https://www.curseforge.com/minecraft/mc-mods/jei">https://www.curseforge.com/minecraft/mc-mods/jei</a></dd>
-                </dl>
-              </dd>
-            </div>
-
-            <div className="modContainer">
-              <dt>Inventory Profiles Next <span>{`${txt("y sus dependencias", "and its dependency")}`}</span></dt>
-              <dd>
-                <dl>
-                  <dt>{`${txt("Descripción", "Description")}`}:</dt>
-                  <dd>{`${txt("Podes ordernar el inventario automaticamente", "you can automatically sort the inventory")}`}</dd>
-
-                  <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
-                  <dd>{`${txt("es util porque te ahorra mucho tiempo si ordenas el inventario", "it's useful because it saves you a lot of time if you sort the inventory")}`}</dd>
-
-                  <dt>{`${txt("Dependencia", "dependency")}`}:</dt>
-                  <dd>Kotlin for Forge</dd>
-                  <dd>libIPN</dd>
-
-                  <dt>Links:</dt>
-                  <dd>
-                    <a href="https://www.curseforge.com/minecraft/mc-mods/inventory-profiles-next">https://www.curseforge.com/minecraft/mc-mods/inventory-profiles-next</a>
-                    <a href="https://www.curseforge.com/minecraft/mc-mods/kotlin-for-forge">https://www.curseforge.com/minecraft/mc-mods/kotlin-for-forge</a>
-                    <a href="https://www.curseforge.com/minecraft/mc-mods/libipn">https://www.curseforge.com/minecraft/mc-mods/libipn</a>
-                  </dd>
-
-                </dl>
-              </dd></div>
-
-            <div className="modContainer">
-              <dt>What the hell is that <span>{`${txt("y sus dependencias", "and its dependency")}`}</span></dt>
-              <dd>
-                <dl>
-                  <dt>{`${txt("Descripción", "Description")}`}:</dt>
-                  <dd>{`${txt("podes ver el objeto que esta en frente de ti", "you can see the item that is in front of you")}`}</dd>
-
-                  <dt>{`${txt("Dependencia", "dependency")}`}:</dt>
-                  <dd>Bad Packets</dd>
-
-                  <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
-                  <dd>{`${txt("es conveniente si queres saber cual es el item que esta en frente tuyo. Incluso puedes asignar una tecla para que te aparezca como craftearlo si tenes 'Just Enough Items'", "it's convinient since you can see the item that is in front of you and you can even assing a key so you can see how to craft the item that is in front of you if you have 'Just Enough Items'")}`}</dd>
-
-                  <dt>Link:</dt>
-                  <dd>
-                    <a href="https://www.curseforge.com/minecraft/mc-mods/wthit-forge">https://www.curseforge.com/minecraft/mc-mods/wthit-forge</a>
-                    <a href="https://www.curseforge.com/minecraft/mc-mods/badpackets">https://www.curseforge.com/minecraft/mc-mods/badpackets</a>
-                  </dd>
-                </dl>
-              </dd>
-            </div>
-
-            <div className="modContainer">
-              <dt>GUI Clock</dt>
-              <dd>
-                <dl>
-                  <dt>{`${txt("Descripción", "Description")}`}:</dt>
-                  <dd>{`${txt("muestra la hora de Minecraft", "it's shows you the Minecraft time")}`}</dd>
-
-                  <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
-                  <dd>{`${txt("si estas en una cueva, podes ver si es de dia o de noche", "if you're in a cave, you can see if daytime or not")}`}</dd>
-
-                  <dt>Link:</dt>
-                  <dd>
-                    <a href="https://www.curseforge.com/minecraft/mc-mods/gui-clock">https://www.curseforge.com/minecraft/mc-mods/gui-clock</a>
-                  </dd>
-                </dl>
-              </dd>
-            </div>
+            <ModContainer
+              name="Just Enough Items"
+              description={`${txt("Podes ver recipientes avanzados que no te muestra en Vanilla Minecraft y se integra con el mod create muy bien", "you can see advanced recipes that are not shown in vanilla Minecraft and it integrates very well with the create mod")}`}
+              link="https://www.curseforge.com/minecraft/mc-mods/jei"
+              reasonWhy={`${txt("Lo vas a necesitar para saber como craftear cosas de mods como el Mod Create o el mod de decoracion", "You're going to need this if you're going to craft things that are related to some of the mods like the create/furniture mod")}`}
+            />
 
 
-            <div className="modContainer">
-              <dt>AppleSkin</dt>
-              <dd>
-                <dl>
-                  <dt>{`${txt("Descripción", "Description")}`}:</dt>
-                  <dd>{`${txt("Mod que te brinda informacion extra de la hambre", "it gives you extra information about hunger")}`}</dd>
-
-                  <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
-
-                  <dd>{`${txt("Te puede servir un poco y se ve un poco cool ver como te va bajando la hambre con mas detalles, y es un mod muy liviano pero no es algo wow", "it may be useful if you want extra information about hunger. It's lightweight but it doesn't do much")}`}</dd>
-                  <dt>Link:</dt>
-                  <dd><a href="https://www.curseforge.com/minecraft/mc-mods/appleskin">https://www.curseforge.com/minecraft/mc-mods/appleskin</a></dd>
-                </dl>
-              </dd>
-            </div>
+            <ModContainer
+              name="Inventory Profiles Next"
+              spanName={`${txt("y sus dependencias", "and its dependency")}`}
+              description={`${txt("Podes ordernar el inventario automaticamente", "you can automatically sort the inventory")}`}
+              link={["https://www.curseforge.com/minecraft/mc-mods/inventory-profiles-next", "https://www.curseforge.com/minecraft/mc-mods/kotlin-for-forge", "https://www.curseforge.com/minecraft/mc-mods/libipn"]}
+              reasonWhy={`${txt("es util porque te ahorra mucho tiempo si ordenas el inventario", "it's useful because it saves you a lot of time if you sort the inventory")}`}
+              dependencies={["Kotlin for Forge", "libIPN"]}
+            />
 
 
-            <div className="modContainer">
-              <dt>Better Third Person</dt>
-              <dd>
-                <dl>
-                  <dt>{`${txt("Descripción", "Description")}`}:</dt>
-                  <dd>{`${txt("hace posible que puedas jugar en 3ra persona", "it makes it so you can play with in 3rd person")}`}</dd>
+            <ModContainer
+              name="What the hell is that"
+              spanName={`${txt("y sus dependencias", "and its dependency")}`}
+              description={`${txt("podes ver el objeto que esta en frente de ti", "you can see the item that is in front of you")}`}
+              link={["https://www.curseforge.com/minecraft/mc-mods/wthit-forge", "https://www.curseforge.com/minecraft/mc-mods/badpackets"]}
+              reasonWhy={`${txt("es conveniente si queres saber cual es el item que esta en frente tuyo. Incluso puedes asignar una tecla para que te aparezca como craftearlo si tenes 'Just Enough Items'", "it's convinient since you can see the item that is in front of you and you can even assing a key so you can see how to craft the item that is in front of you if you have 'Just Enough Items'")}`}
+              dependencies={["Bad Packets"]}
+            />
 
-                  <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
-                  <dd>{`${txt("bastante inutil si jugas en primera persona", "really useful if you play in first person")}`}</dd>
 
-                  <dt>Link:</dt>
-                  <dd>
-                    <a href="https://www.curseforge.com/minecraft/mc-mods/better-third-person">https://www.curseforge.com/minecraft/mc-mods/better-third-person</a>
-                  </dd>
-                </dl>
-              </dd>
-            </div>
+            <ModContainer
+              name="GUI Clock"
+              description={`${txt("muestra la hora de Minecraft", "it's shows you the Minecraft time")}`}
+              link="https://www.curseforge.com/minecraft/mc-mods/gui-clock"
+              reasonWhy={`${txt("si estas en una cueva, podes ver si es de dia o de noche", "if you're in a cave, you can see if daytime or not")}`}
+            />
 
-            <div className="modContainer">
-              <dt>Tiny Item Animations</dt>
-              <dd>
-                <dl>
-                  <dt>{`${txt("Descripción", "Description")}`}:</dt>
-                  <dd>{`${txt("agrega una animacion pequenia", "add a small animation")}`}</dd>
 
-                  <dt>{`${txt("La razon por la cual este mod estuvo ordenado en este puesto", "The reason why this mod was sorted in this way")}`}:</dt>
-                  <dd>{`${txt("no agrega casi nada al gameplay asi que esta al ultimo puesto", "it doesn't add anything important to the game so its sorted as last")}`}</dd>
+            <ModContainer
+              name="AppleSkin"
+              description={`${txt("Mod que te brinda informacion extra de la hambre", "it gives you extra information about hunger")}`}
+              link="https://www.curseforge.com/minecraft/mc-mods/appleskin"
+              reasonWhy={`${txt("Te puede servir un poco y se ve un poco cool ver como te va bajando la hambre con mas detalles, y es un mod muy liviano pero no es algo wow", "it may be useful if you want extra information about hunger. It's lightweight but it doesn't do much")}`}
+            />
 
-                  <dt>Link:</dt>
-                  <dd>
-                    <a href="https://www.curseforge.com/minecraft/mc-mods/tiny-item-animations">https://www.curseforge.com/minecraft/mc-mods/tiny-item-animations</a>
-                  </dd>
-                </dl>
-              </dd>
-            </div>
+            <ModContainer
+              name="Better Third Person"
+              description={`${txt("hace posible que puedas jugar en 3ra persona", "it makes it so you can play with in 3rd person")}`}
+              link="https://www.curseforge.com/minecraft/mc-mods/better-third-person"
+              reasonWhy={`${txt("bastante inutil si jugas en primera persona", "really useful if you play in first person")}`}
+            />
 
+
+            <ModContainer
+              name="Tiny Item Animations"
+              description={`${txt("agrega una animacion pequenia", "add a small animation")}`}
+              link="https://www.curseforge.com/minecraft/mc-mods/tiny-item-animations"
+              reasonWhy={`${txt("no agrega casi nada al gameplay asi que esta al ultimo puesto", "it doesn't add anything important to the game so its sorted as last")}`}
+            />
           </dl>
         </section>
       </details>
